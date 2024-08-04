319b8254bde3447eba8707fe62255782cfd70d1c
João Batista <joao42lbatista@gmail.com>
2024-08-04 08:48:29 -0300

    Update kdiff3-junest.sh

--- recipes/kdiff3-junest.sh
+++ recipes/kdiff3-junest.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+# Last update: 04/08/2024
+
 # NAME OF THE APP BY REPLACING "SAMPLE"
 APP=kdiff3
 BIN="$APP" #CHANGE THIS IF THE NAME OF THE BINARY IS DIFFERENT FROM "$APP" (for example, the binary of "obs-studio" is "obs")
